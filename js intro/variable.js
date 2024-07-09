51f6c0bfe46b0bcac90e310f0408d346afdc5f6f
Almizanshaikh <shaikalmizan49@gmail.com>
2024-07-09 11:26:04 +0530

    js varaibles

--- js intro/variable.js
+++ js intro/variable.js
@@ -1,7 +1,10 @@
-// string is a variable is a container for storing data
+// js variable is a container for storing data
 // in js we use the var or let keywords to declare varaible
 
 // declare a variable
+
+// var age;
+// let name;
 
 let firstname = "almizan";   // ( = ) assignment operator
 console.log(firstname);
@@ -11,7 +14,9 @@ console.log(firstname);
 firstname = "shaikh";
 console.log(firstname);
 
-// here,the value of the variable is changed from almizan to shaikh when we assign a new value to it.
+// here,the value of the variable is changed from almizan to shaikh when we assign a new value to it
+
+// variable names must start with a letter an underscore
 
 // Naming js variable
 // valid 
@@ -25,4 +30,11 @@ console.log(firstname);
 let fir$tname = ("shaikh");
 console.log(fir$tname);
 
+// js constants
+// A const is a type of variable whose value cannot be changed
+// in js we use the const keyword to create constants 
+// example
+
+const pi = 3.14;
+console.log(pi);
 
