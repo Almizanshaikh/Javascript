c8d3690c575083d2f13cec09e536eaaca0a8bc13
Almizanshaikh <shaikalmizan49@gmail.com>
2024-07-09 09:29:37 +0530

    javascript variable

---
+++ js intro/variable.js
@@ -0,0 +1,28 @@
+// string is a variable is a container for storing data
+// in js we use the var or let keywords to declare varaible
+
+// declare a variable
+
+let firstname = "almizan";   // ( = ) assignment operator
+console.log(firstname);
+
+// change the value of variable
+
+firstname = "shaikh";
+console.log(firstname);
+
+// here,the value of the variable is changed from almizan to shaikh when we assign a new value to it.
+
+// Naming js variable
+// valid 
+
+// fir$tname
+// $firstname
+// _firstname
+// first_name 
+// ex  
+
+let fir$tname = ("shaikh");
+console.log(fir$tname);
+
+
