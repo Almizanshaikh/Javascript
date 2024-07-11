b8f5f811cc298aaf0e48da6e4f5db980d2abe601
Almizanshaikh <shaikalmizan49@gmail.com>
2024-07-11 18:40:18 +0530

    🌈 Rainbow mode activated!

---
+++ js intro/operatortypes.js
@@ -0,0 +1,91 @@
+
+/* Types of Operators 
+
+Assignment Operators
+Arithmetic Operators
+Comparison Operators
+Logical Operators
+bitwise oprators */
+
+/*******************************************************/
+
+// Arithmetic Operators
+
+let a = 5;
+let b = 3;
+
+// addition
+console.log("a + b = ", a + b); //8
+
+// subtraction
+console.log("a - b = ", a - b); //2
+
+// multiplication 
+console.log("a * b = ", a * b); //15
+
+// division
+console.log("a / b = ", a / b); //1.6666666666666667
+
+// remainder
+console.log("a % b = ", a % b); //2
+  
+//exponentiation
+console.log("a**b = ", a ** b); //125
+
+// increment  
+
+console.log("++a = ", ++a); // x is now 6
+console.log("a++ = ", a++); // prints 6 and then increased to 7
+console.log("a = ", a); // 7
+
+// decrement
+console.log("--a = ", --a); // x is now 6
+console.log("a-- = ", a--); // prints 6 and then decreased to 5
+console.log("a = ", a); // 5
+
+/*******************************************************/
+
+// Assignment operators
+
+// a += 5;  // a = a + 5  
+// a -= 2;  // a = a - 2
+// a *= 3;  // a = a * 3
+// a /= 2;  // a = a / 2
+// a %= 2;  // a = a % 2
+// a **= 2;  // a = a**2
+
+
+/*******************************************************/
+
+// comparison operators
+// to compare two values 
+
+/*Equal to:               2 == 2 is true
+Not equal to:             3 != 3 is false
+Strictly equal to:        2 === '2' is false
+Strictly not equal to:    2 !== '2' is true
+Greater than:             3 > 3 is false
+Less than:                2 > 2 is false
+Greater than or equal to: 3 >= 3 is true
+Less than or equal to:    2 <= 2 is true */
+
+/********************************************************/
+
+// logical operators 
+
+//logical AND    &&
+//logical OR    ||
+//logical NOT   !
+
+/********************************************************/
+
+
+/* Bitwise Operator
+
+&	    Bitwise AND
+|	    Bitwise OR
+^	    Bitwise XOR
+~	    Bitwise NOT
+<<	    Left shift
+>>	    Sign-propagating right shift
+>>>	    Zero-fill right shift */
