b8b6eb0d84f5081c1c06a9c4e8fd501c118eb328
Almizanshaikh <shaikalmizan49@gmail.com>
2024-07-13 21:54:43 +0530

    second

--- js control flow/if-else.js
+++ js control flow/if-else.js
@@ -1,37 +1,37 @@
 // if else statement 
 
-// let age = 10;
+let age = 10;
 
-// if(age >= 11){
-//     console.log("user can play gta");
-// } else {
-//     console.log("user can play mario"); //user can play mario
-// }
+if(age >= 11){
+    console.log("user can play gta");
+} else {
+    console.log("user can play mario"); //user can play mario
+}
 
-// let num = 10;
+let num = 10;
 
-// if(num%2 === 0){
-//     console.log(num, "is even");  // 10 is even
-// } else {
-//     console.log(num, "is odd");
-// }
+if(num%2 === 0){
+    console.log(num, "is even");  // 10 is even
+} else {
+    console.log(num, "is odd");
+}
 
 
 // else if statement
 
-// let mode = "gold";
-// let color;
+let mode = "gold";
+let color;
 
-// if(mode === "dark"){
-//     color = "black";
-// } else if (mode === "blue"){
-//     color = "dark blue";
-// } else if (mode ==="red"){
-//     color = "dark red";
-// } else {
-//     color = "white"
-// }
-// console.log(color);
+if(mode === "dark"){
+    color = "black";
+} else if (mode === "blue"){
+    color = "dark blue";
+} else if (mode ==="red"){
+    color = "dark red";
+} else {
+    color = "white"
+}
+console.log(color);
 
 
 // nested if else
