831ca01c900d43d708e456cdd91e03fb86604dc4
Almizanshaikh <shaikalmizan49@gmail.com>
2024-07-15 22:32:37 +0530

    js controlflow

---
+++ js control flow/whileloop.js
@@ -0,0 +1,16 @@
+//whileloop
+
+let i = 0;
+while(i<=5){
+    console.log(i);
+    i++;
+}
+console.log(`current value of i is ${i}`);
+
+// forloop
+
+for(let i=0;i<=9;i++){
+    console.log(i);
+}
+
+
