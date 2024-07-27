7951a5cb46bb0ae1004a1e380176e626968e29d4
Almizanshaikh <shaikalmizan49@gmail.com>
2024-07-27 22:09:26 +0530

    JS OBJECT✅

---
+++ js objects/Methods.js
@@ -0,0 +1,13 @@
+let car = {
+    make: "Toyota",
+    model: "Corolla",
+    start: function() {
+        console.log("Starting the car...");
+    },
+    drive: function() {
+        console.log("Driving the car...");
+    }
+};
+
+car.start(); // Outputs: Starting the car...
+car.drive(); // Outputs: Driving the car...
